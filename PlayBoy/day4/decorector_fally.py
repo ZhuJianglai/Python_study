6c49c3d71b6cf2c9cdf1970f244729c9f9c3468b
zhujianglai <316967266@qq.com>
2018-10-12 00:04:21 +0800

    1

--- PlayBoy/day4/decorector_fally.py
+++ PlayBoy/day4/decorector_fally.py
@@ -2,23 +2,32 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*-
 
-import time
+
 user,passwd='alex','abc123'
 def auth(func):
     def wrapper(*args,**kwargs):
-        username=input("Username").strip()
-        password=input("Password").strip()
+        username=input("Username:").strip()
+        password=input("Password:").strip()
         if user ==username and passwd==password:
-            func
-            dsad das
+          print("\33[32;1mUser has passed authentiacation\033[0m")
+          func(*args,**kwargs)
+        else:
+          exit("\33[32;1mInvalid username or password\033[0m")
+    return wrapper
 
-
+@auth
 def index():
     print("welcome to index page")
 
+@auth
 def home():
     print("welcome to home page")
-
+    return "from home"
+@auth
 def bbs():
         print("welcome to bbs page")
+
+index()
+print(home())
+bbs()
 
