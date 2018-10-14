d66609986d38d6787f6e200fd6e4730c68476272
zhujianglai <316967266@qq.com>
2018-10-15 00:05:15 +0800

    1

--- PlayBoy/day4/decorector_fally.py
+++ PlayBoy/day4/decorector_fally.py
@@ -4,28 +4,50 @@
 
 
 user,passwd='alex','abc123'
-def auth(func):
-    def wrapper(*args,**kwargs):
-        username=input("Username:").strip()
-        password=input("Password:").strip()
-        if user ==username and passwd==password:
-          print("\33[32;1mUser has passed authentiacation\033[0m")
-          res=func(*args,**kwargs)
-          print("---after authentication")
-          return res
-        else:
-          exit("\33[32;1mInvalid username or password\033[0m")
-    return wrapper
+# def auth(func):
+#     def wrapper(*args,**kwargs):
+#         username=input("Username:").strip()
+#         password=input("Password:").strip()
+#         if user ==username and passwd==password:
+#           print("\33[32;1mUser has passed authentiacation\033[0m")
+#           res=func(*args,**kwargs)
+#           print("---after authentication")
+#           return res
+#         else:
+#           exit("\33[32;1mInvalid username or password\033[0m")
+#     return wrapper
+#添加参数后
+user,passwd='alex','abc123'
+def auth(auth_type):
+    def out_wrapper(func):
+        def wrapper(*args,**kwargs):
+            if auth_type=="local":
+                username=input("Username:").strip()
+                password=input("Password:").strip()
+                if user ==username and passwd==password:
+                  print("\33[32;1mUser has passed authentiacation\033[0m")
+                  res=func(*args,**kwargs)
+                  print("---after authentication")
+                  return res
+                else:
+                  exit("\33[32;1mInvalid username or password\033[0m")
+            elif auth_type=="ldap":
+                print("搞毛线，不会了。。。。")
+        return wrapper
+    return out_wrapper
 
 
 def index():
     print("welcome to index page")
 
-@auth
+@auth(auth_type="local") #添加参数
+# @auth
 def home():
     print("welcome to home page")
     return "from home"
-@auth
+
+# @auth
+@auth(auth_type="ldap") #添加参数
 def bbs():
         print("welcome to bbs page")
 
