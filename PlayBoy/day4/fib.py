0f67d105049be99f2e7e9dc1c628b785874e4709
zhujianglai <316967266@qq.com>
2018-10-18 23:33:45 +0800

    斐波那契数列

---
+++ PlayBoy/day4/fib.py
@@ -0,0 +1,15 @@
+# author：zhu
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+#斐波那契
+
+def fib(max):
+    n,a,b=0,0,1
+    while n <max:
+        print(b)
+        a,b=b,a+b
+        n=n+1
+    return 'done'
+
+
+fib(10)
