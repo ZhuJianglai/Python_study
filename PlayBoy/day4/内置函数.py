993a74780bef3520a5f6d39f623b5085845dc74f
zhujianglai <316967266@qq.com>
2018-11-12 23:48:48 +0800

    内置函数

---
+++ PlayBoy/day4/内置函数.py
@@ -0,0 +1,10 @@
+# author：zhu
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+
+#内置函数
+#参考说明https://docs.python.org/3.7/library/functions.html
+print(all([1,-1,3]))
+print(any([]))
+print(bin(2)) #将数字转换二进制
