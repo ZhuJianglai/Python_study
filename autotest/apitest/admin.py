5137a3382e1d7101eea3e0d082bcf994e1923836
ZhuJianglai <316967266@qq.com>
2020-05-28 15:25:02 +0800

    20200528处理Apistep，Apitest

--- autotest/apitest/admin.py
+++ autotest/apitest/admin.py
@@ -3,13 +3,22 @@ from apitest.models import Apitest,Apistep
 # Register your models here.
 
 #https://docs.djangoproject.com/en/dev/ref/contrib/admin/#working-with-many-to-many-models
-class ApistepAdmin(admin.TabularInline):
-    list_display=['apiname','apiurl','apiparamvalue','apimethod','apiresult','apistatus','create_time','id','apitest']
+# class ApistepAdmin(admin.TabularInline):
+#     list_display=['apiname','apiurl','apiparamvalue','apimethod','apiresult','apistatus','create_time','id','apitest']
+#     model = Apistep
+#     extra = 1
+
+
+class ApistepInline(admin.TabularInline):
     model = Apistep
     extra = 1
 
+class ApistepAdmin(admin.ModelAdmin):
+    list_display=['apiname','apiurl','apiparamvalue','apimethod','apiresult','apistatus','create_time','id','apitest']
+
 class ApitestAdmin(admin.ModelAdmin):
     list_display = ['apitestname','apitestdesc','apitester','apitestresult','create_time','id']
-    inlines = [ApistepAdmin]
+    inlines = [ApistepInline]
 
-admin.site.register(Apistep,ApitestAdmin)
+admin.site.register(Apistep)
+admin.site.register(Apitest)
