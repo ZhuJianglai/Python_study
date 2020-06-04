31159576c127b1ee657cccaf5165f208786c435d
ZhuJianglai <316967266@qq.com>
2020-06-04 20:01:38 +0800

    20200604-apiauto——testcase

--- autotest/apitest/apiauto_testcase.py
+++ autotest/apitest/apiauto_testcase.py
@@ -46,8 +46,6 @@ def interfaceTest(case_list):
         except Exception as e:
             return '测试用例格式不正确%s'%e
 
-
-
         if param=='':
             new_url='http://'+'api.test.com.cn'+url
         elif param=='null':
@@ -62,12 +60,44 @@ def interfaceTest(case_list):
             if "=" in urlParam(param):
                 date=None
                 print(str(case_id)+'request is get'+new_url.encode('utf-8')+'?'+urlParam(param).encode('utf-8'))
-                results=requests.get(new_url+'?'+urlrlParm(param),date,headers=headers).text
+                results=requests.get(new_url+'?'+urlParam(param),date,headers=headers).text
                 print('response is get'+results.encode('utf-8'))
                 responses.append(results)
                 res=readRes(results,'')
             else:
-                print('request is get'+new_url+'body is' +ur
+                print('request is get'+new_url+'body is' +urlParam(param))
+                date=None
+                req=urllib.request.Request(url=new_url,date=date,headers=headers,method='GET')
+                results=urllib.request.urlopen(req).read()
+                print('response is get')
+                print(results)
+                res=readRes(results,res_check)
+            if 'pass'==res:
+                writeResult(case_id,'1')
+                res_flags.append('pass')
+            else:
+                res_flags.append('fail')
+                writeResult(case_id,'0')
+            if method.upper() =='PUT':
+                headers ={'Host':HOSTNAME,'Connection':'keep-alive','CredentialId':id,'Content-type':'appliacation/json'}
+                body_data=param
+                results=requests.put(url=url,date=body_data,headers=headers)
+                responses.append(results)
+                res=readRes(results,res_check)
+                if 'pass'==res:
+                    writeResult(case_id,'pass')
+                    res_flags.append('pass')
+                else:
+                    writeResult(case_id,'fail')
+                    res_flags.append('fail')
+                    writeBug(case_id,interface_name,new_url,results,res_check)
+                try:
+                    preOrderSN(results)
+                except:
+                    print('ok')
+            if method.upper()='PATCH':
+                headers ={'Authorization':'CredentialId'+id,'Content-type':'appliacation/json'}
+
 
 
 
@@ -133,8 +163,37 @@ def writeResult(case_id,result):
     result=result.encode('utf-8')
     now =time.strftime("%Y-%m-%d %H:%M:%S")
     sql="UPDATE apitest_apistep SET apitest_apistep.apistatus=%s WHERE apitest_apistep.Apitest_id=%s"
+    parma=(result,case_id)
+    print('api autotest result is '+result.decode())
+    coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host='127.0.0.1',chartset='utf-8')
+    cursor=coo.cursor()
+    cursor.execute(sql,parma)
+    coo.commit()
+    cursor.close()
+    coo.close()
 
 
 
+def writeBug(bug_id,interface_name,request,response,res_check):
+    interface_name=interface_name.encode('utf-8')
+    res_check=res_check.encode('utf-8')
+    request=request.encode('utf-8')
+    now=time.strftime("%Y-%m-%d %H:%M:%S")
+    bug_name=str(bug_id)+"_"+interface_name.decode()+'_出错了'
+    bugdetail='[请求数据]<br />'+request.decode()+'<br />'+'<br />'+'[预期结果]<br />'+res_check.decode()+'<br />'+'<br />'+'[响应数据]<br />'+response.decode()
+    print(bugdetail)
+    sql='INSERT INTO bug_bug(bugname,bugdetail,bugstatus,buglevel,bugcreater,bugassign create_time,Product_id) VALUES(%s,%s,1,1,'+'zhuning','zhuning'+',%s,2);'%(bug_name,pymysql.escape_string(bugdetail),now)
+    coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host='127.0.0.1',chartset='utf-8')
+    cursor=coo.cursor()
+    cursor.execute(sql)
+    coo.commit()
+    cursor.close()
+    coo.close()
+
+
+if __name__=='__main__':
+    readSQLcase()
+    print('Done')
+    time.sleep(1)
 
 
