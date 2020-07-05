64d56723d67783149ebb8cf78eaf8c8b874ba2ed
ZhuJianglai <316967266@qq.com>
2020-07-05 23:59:35 +0800

    20200705set_manage添加翻页功能

--- autotest/set/setviews.py
+++ autotest/set/setviews.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from set.models import Set
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
+
+
 
 # Create your views here.
 #设置管理
 def set_manage(request):
     username=request.session.get('user','')
     set_list=Set.objects.all()
+    paginator=Paginator(set_list,8)
+    page=request.GET.get('page',1)
+    currentPage=int(page)
+    try:
+        set_list=paginator.page(page)
+    except PageNotAnInteger:
+        set_list=paginator.page(1)
+    except EmptyPage:
+        set_list=paginator.page(paginator.num_pages)
+
     return render(request,'set_manage.html',{'user':username,'sets':set_list})
 
 
@@ -23,3 +37,4 @@ def setsearch(request):
     search_setname=request.GET.get("setname","")
     set_list=Set.objects.filter(setname__icontains=search_setname)
     return render(request,'set_manage.html',{'user':username,"sets":set_list})
+
