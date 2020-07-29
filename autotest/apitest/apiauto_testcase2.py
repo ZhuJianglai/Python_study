b9e84459bac7302c1636e51894457c4f42e65213
ZhuJianglai <316967266@qq.com>
2020-07-29 23:31:54 +0800

    20200729(单一接口测试用例-P201)

--- autotest/apitest/apiauto_testcase2.py
+++ autotest/apitest/apiauto_testcase2.py
@@ -10,9 +10,10 @@ import re
 import json
 import requests
 import time
+import urllib
 def test_readSQLcase():
     sql="select id,apiname,apiurl,apimethod,apiparamvalue,apiresult,apistatus from autotest.apitest_apis"
-    coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port='3306',host='127.0.0.1',charser='utf8')
+    coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host='127.0.0.1',charset='utf8')
     cursor=coo.cursor()
     aa=cursor.execute(sql)
     info=cursor.fetchmany(aa)
@@ -48,9 +49,38 @@ def interfaceTest(case_list):
             url = strinfo.sub(str(seturl('seturl')), url)
             new_url='http://'+'127.0.0.1'+url
             request_url.append(new_url)
-        if method.upper()=='GET':
+        print(method.upper())
+        if method.upper()=='1':
+        # if method.upper() == 'GET':
             headers={'Authorizations':'','Content-Type':'application/json'}
-            if "=" in ur
+            if "=" in urlParam(param):
+                data=None
+                print(str(case_id)+'request is get'+new_url.encode('utf-8')+'?'+urlParam(param).encode('utf-8'))
+                result=requests.get(new_url+'?'+urlParam(param),data,headers=headers).text
+                print('response is get'+result.encode('utf-8'))
+                response.append(result)
+                res=readRes(result,'')
+            else:
+                print('request is get'+new_url+'body is '+urlParam(param))
+                data=None
+                req=urllib.request.Request(url=new_url,data=data,headers=headers,method='GET')
+                try:
+                    result=urllib.request.urlopen(req).read()
+                    print('response is get')
+                    print(result)
+                except Exception as e:
+                    return caseWriteResult(case_id,0)
+                res=readRes(result,res_check)
+                if 'pass' == res:
+                    res_flags.append('pass')
+                    writeResult(case_id,'1')
+                    caseWriteResult(case_id,'1')
+                else:
+                    res_flags.append('fail')
+                    writeResult(case_id,'0')
+                    caseWriteResult(case_id,'0')
+
+
 
 
 
@@ -81,7 +111,7 @@ def CredentialId():
     id=pm.group(1)
 
 def seturl(set):
-    global seturl()
+    global setvalue
     sql="select setname,setvalue from set_set"
     coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host='127.0.0.1',charset='utf8')
     cursor=coo.cursor()
@@ -90,9 +120,13 @@ def seturl(set):
     print(info)
     coo.commit()
     cursor.close()
-    if info[0][0]==set:
-        setvalue=info[0][1]
-        print(setvalue)
+    for ii in info:
+    # if info[0][0]==set:
+    #     setvalue=info[0][1]
+    #     print(setvalue)
+        if ii[0]==set:
+            setvalue=ii[1]
+            print(setvalue)
     return setvalue
 
 def writeResult(case_id,result):
@@ -126,6 +160,24 @@ def writeBug(bug_id,interface_name,request,response,res_check):
     interface_name=interface_name.encode('utf-8')
     res_check=res_check.encode('utf_8')
     request=request.encode('utf-8')
+    now=time.strftime('%Y-%m-%d %H:%M:%S')
+    bug_name=str(bug_id)+'_'+interface_name.decode()+'_出错了'
+    bugdetail='[请求数据]<br />'+request.decode()+'<br/>'+'[预期结果]<br />'+res_check.decode()+'<br />'+'<br />'+'[响应数据]<br />'+'<br />'+response.decode()
+    print(bugdetail)
+    sql="insert into bug_bug ('bug_name','bugdetail','bugstatus','buglevel','bugcreater','bugassign',create_time','Product_id')values(%s,%s,'1','1','邹辉','邹辉',%s,'2)"%(bug_name,pymysql.escape_string(bugdetail),now)
+    coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host='127.0.0.1',charset='utf8')
+    cursor=coo.cursor()
+    cursor.execute(sql)
+    coo.commit()
+    cursor.close()
+    coo.close()
+
+
+
+if __name__=='__main__':
+    test_readSQLcase()
+    print('Done')
+    time.sleep(1)
 
 
 
