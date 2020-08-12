f83fbf4429e53dc18492da1b89bf398217b2f34a
ZhuJianglai <316967266@qq.com>
2020-08-12 23:12:47 +0800

    20200812(网页自动化P225)

---
+++ autotest/fconfig.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-8-12 22:54  
+# @Author  : Administrator
+# @FileName: fconfig.py.py
+# @Software: PyCharm
+# !/usr/bin/python
+
+
+import configparser
+import os
+
+def getConfig(section,key):
+    config=configparser.ConfigParser()
+    path=os.path.split(os.path.realpath(__file__))[0]+'/settings.ini'
+    config.read(path)
+    return config.get(section,key)
+
+
+if __name__=='__main__':
+    print(getConfig('database','host'))
+
--- autotest/webtest/webauto_testcase2.py
+++ autotest/webtest/webauto_testcase2.py
@@ -5,31 +5,44 @@
 # @Software: PyCharm 
 # !/usr/bin/python
 
+#20200812加入Unittest和HTMLTestRunner框架生成报告
+
 import os
 import time
 import unittest
 import pymysql
 import fconfig
 from selenium import webdriver
+import HTMLTestRunner
+
+
+
 
 PATH=lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
 global driver
 
 HOSTNAME='127.0.0.1'
+class search(unittest.TestCase):
+    def setUp(self) :
+        time.sleep(5)
 
-def readSQLcase():
-    sql="select id,webteststep,webfindmethod,webevelement,weboptmethod,webtestdata,webassertdata,webtestresult from autotest.webtest_webcasestep where autotest.webtest_webcasestep.webcase_id=3 order by id asc"
-    coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host=HOSTNAME,chartset='utf8')
-    cursor=coo.cursor()
-    aa=cursor.execute(sql)
-    info=cursor.fetchmany(aa)
-    for ii in info:
-        case_list=[]
-        case_list.append(ii)
-        webtestcase(case_list)
-    coo.commit()
-    cursor.close()
-    coo.close()
+
+    def test_readSQLcase(self):
+        sql="select id,webteststep,webfindmethod,webevelement,weboptmethod,webtestdata,webassertdata,webtestresult from autotest.webtest_webcasestep where autotest.webtest_webcasestep.webcase_id=3 order by id asc"
+        coo=pymysql.connect(user='root',passwd='1234567a',db='autotest',port=3306,host=HOSTNAME,chartset='utf8')
+        cursor=coo.cursor()
+        aa=cursor.execute(sql)
+        info=cursor.fetchmany(aa)
+        for ii in info:
+            case_list=[]
+            case_list.append(ii)
+            webtestcase(case_list)
+        coo.commit()
+        cursor.close()
+        coo.close()
+
+    def tearDown(self):
+        self.driver.quit()
 
 
 def webtestcase(case_list):
@@ -60,7 +73,7 @@ if __name__=='__main__':
      driver=webdriver.Chrome()
      driver.get('http://www.baidu.com')
      time.sleep(1)
-     readSQLcase()
+
      time.sleep(1)
      driver.quit()
 
